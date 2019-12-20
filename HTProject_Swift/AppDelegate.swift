2fadcae028c4ee10bb8e5721c9662535d164c586
HemingTai <heming_tai@163.com>
2019-12-20 16:14:09 +0800

    update

--- HTProject_Swift/AppDelegate.swift
+++ HTProject_Swift/AppDelegate.swift
@@ -12,7 +12,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    /********************** OC的协议与Swift的协议有什么不同？为什么说Swift是面向协议编程？************************
+     * OC中只有类可以遵循协议，并实现协议方法，struct不可以
+     * Swift中协议可以支持扩展，struct也可以遵守协议
+     *
+     * 协议和协议扩展比基类有三个明显的优点：
+     * 1、类型可以遵守多个协议但是只有一个基类。这意味着类型可以随意遵守任何想要特性的协议，而不需要一个巨大的基类。
+     * 2、不需要知道源码就可以使用协议扩展添加功能。这意味着我们可以任意扩展协议，包括swift内置的协议，而不需要修改基类的源码。
+     * 3、协议可以被类、结构体和枚举遵守，而类层级约束为类类型。
+     */
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
